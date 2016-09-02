e8dc9a03ff0f3bf659b64ff2e1264bb69c5d80ec
cschmitz <schmitz.chris@gmail.com>
2016-09-02 13:00:04 -0500

    wip.

--- prototypes/twitterstream/index.js
+++ prototypes/twitterstream/index.js
@@ -5,11 +5,22 @@ const twitterApi = new Twitter(twitterCredentials.consumerKey, twitterCredential
     console.log(arguments)
 })
 
-twitterApi.getHomeLineTweets(twitterCredentials.accessToken, twitterCredentials.accessTokenSecret, (error, result) => {
+
+twitterApi.getUsersTweets('everycolorbot', 1, twitterCredentials.accessToken, twitterCredentials.accessTokenSecret, (error, result) => {
     if (error) {
         console.error(error)
     }
     if (result) {
-        console.log(result)
+        console.log('got result')
+        let tweet = JSON.parse(result)[0] // outputs an array of json objects
+        return tweet
     }
 })
+
+function getColorAndLink(tweet){
+    const expression = /^0x([\w\d]{6})\s([\w\d:/].*)/
+    if(!expression.test(tweet)){
+        return {color: null, link: null}
+    }
+    return {color: expression.exec(tweet)[1], link: expression.exec(tweet)[2]}
+}
