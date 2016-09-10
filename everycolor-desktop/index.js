7a4c6eb64b24f75373e8aeb5aecc1947d42a6303
cschmitz <schmitz.chris@gmail.com>
2016-09-10 15:01:22 -0500

    Adds working twitter stream. Pulls in chalk for better console log coloring.

--- everycolor-desktop/index.js
+++ everycolor-desktop/index.js
@@ -1,8 +1,38 @@
-const everyColorBot = require('./lib/QueriesEveryColorBot')
+// const everyColorBot = require('./lib/QueriesEveryColorBot')
 const desktopImageCreator = require('./lib/CreatesDesktopImages')
 const desktopSetter = require('./lib/SetsDesktopImages')
+const Twitter = require('twitter')
+const credentials = require('./credentials')
+const chalk = require('chalk')
+
+let client = new Twitter({
+    consumer_key: credentials.twitter.consumer_key,
+    consumer_secret: credentials.twitter.consumer_secret,
+    access_token_key: credentials.twitter.access_token_key,
+    access_token_secret: credentials.twitter.access_token_secret
+})
+
+let stream = client.stream('statuses/filter', {
+    follow: '243730082,1909219404'
+})
+
+stream.on('data', event => {
+    let tweet = event.text.match(/^0x[\w\d]{6}(?=\s)/)
+    if(tweet){
+        console.log(chalk.green(`we got a color from the bot: ${tweet[0]}`))
+    } else {
+        console.log(chalk.red(`we got something other than a tweet from the bot: ${event.text}`))
+    }
+
+})
+
+stream.on('error', error => {
+    console.error(error)
+})
 
 
-let color = everyColorBot.getLatestColor()
-let filename = desktopImageCreator.createImageForColor(color)
-desktopSetter.setDesktopBackground(filename)
+
+
+// let color = everyColorBot.getLatestColor()
+// let filename = desktopImageCreator.createImageForColor(color)
+// desktopSetter.setDesktopBackground(filename)
--- everycolor-desktop/lib/QueriesEveryColorBot.js
+++ everycolor-desktop/lib/QueriesEveryColorBot.js
@@ -1,3 +1,13 @@
+const Twitter = require('twitter')
+const credentials = require('../credentials')
+
+let client = new Twitter({
+    consumer_key: credentials.twitter.consumer_key,
+    consumer_secret: credentials.twitter.consumer_secret,
+    access_token_key: credentials.twitter.access_token_key,
+    access_token_secret: credentials.twitter.access_token_secret
+})
+
 function listenForLatestTweet(){
     console.log("here's the latest tweet")
 }
