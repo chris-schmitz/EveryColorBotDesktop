7f571d17e61bdfa7714ed0200c71d9549c51a2a5
cschmitz <schmitz.chris@gmail.com>
2016-09-19 22:40:37 -0500

    Adds ability to list captured colors.

--- prototypes/command-line-interaction/index.js
+++ prototypes/command-line-interaction/index.js
@@ -1,18 +1,37 @@
 #!/usr/bin/env node
 
 const program = require('commander')
+const fs = require('fs')
+const co = require('co')
+const chalk = require('chalk')
+
+function getFileList(){
+    return new Promise((resolve,reject) => {
+        fs.readdir('../../everycolor-desktop/pngWorkspace/', (error, files) => {
+            if(error) reject(error)
+            resolve(files)
+        })
+
+    })
+}
 
 program
-    .command('list-files', 'Lists files pulled by everycolorbot.')
+    .command('list-files')
     .action(() => {
-        console.log('listing the files')
+        co(function *(){
+            let files = yield getFileList()
+            let filtered = files.filter(file => {
+                return file !== "base.png" && file[0] !== "."
+            }).reduce((previous, current) => `${previous}\n${current}`)
+            console.log(chalk.blue("Colors saved from everycolorbot tweets:\n") + filtered)
+        })
     })
 
-program
-    .command('set-background <file>', 'Sets the desktop to the given file.')
-    .action(file => {
-        console.log('set the background to the file: %s.', file)
-    })
+// program
+//     .command('set-background <file>', 'Sets the desktop to the given file.')
+//     .action(file => {
+//         console.log('set the background to the file: %s.', file)
+//     })
 
 program.parse(process.argv)
 
