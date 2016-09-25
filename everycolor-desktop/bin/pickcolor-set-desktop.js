132bc71425b2e27c010a486eb92a09a1d8931fad
Christopher Schmitz <schmitz.chris@gmail.com>
2016-09-25 16:20:14 -0500

    Refactors set desktop "bin" and library file to use generators and promises for the async logic.

--- everycolor-desktop/bin/pickcolor-set-desktop.js
+++ everycolor-desktop/bin/pickcolor-set-desktop.js
@@ -4,6 +4,8 @@ const fs = require('fs')
 const desktop = require('../lib/SetsDesktopImages')
 const notifier = require('../lib/NotifiesUser')
 const path = require('path')
+const co = require('co')
+const chalk = require('chalk')
 
 program
     .parse(process.argv)
@@ -14,14 +16,25 @@ if(process.platform !== 'darwin'){
 
 const filepath = program.args[0]
 
-fs.stat(filepath, (error, stats) => {
-    if(error) throw new Error(error)
+function fileExists(filepath){
+    return new Promise((resolve, reject) => {
+        fs.stat(filepath, (error, stat) => {
+            if(error) {
+                reject(new Error(error))
+            }
+            resolve(stat)
+        })
+    })
+}
 
-    desktop.setDesktopBackground(filepath)
-    // Come back and fix this code so that we handle the notification only on a successful set.
-    // The only reason I'm not doing this right now is because I have maybe 10 mins before I have
-    // to head out for brunch :P Prob another good spot for a generator function so you don't 
-    // have to go another level in with a callback. 
-    let filename = path.basename(filepath)
-    notifier.notifyUser('Desktop Background Set', message = `Desktop set to ${filename}`)
+co(function *(){
+    let exists = yield fileExists(filepath)
+    let backgroundSet = yield desktop.setDesktopBackground(filepath)
+
+    console.log(chalk.green(backgroundSet))
+
+    let fileName = path.basename(filepath)
+    notifier.notifyUser('Desktop Background Set', `Desktop set to ${fileName}`)
+}).catch(error => {
+    console.error(chalk.red(error))
 })
--- everycolor-desktop/lib/SetsDesktopImages.js
+++ everycolor-desktop/lib/SetsDesktopImages.js
@@ -9,24 +9,24 @@ const chalk = require('chalk')
  */
 function setDesktopBackground(filePath, desktopNumber = [1]){
     // future refactor: make this cross platform.
+    return new Promise((resolve, reject) => {
 
-    if(!Array.isArray(desktopNumber)){
-        desktopNumber = [desktopNumber]
-    }
-
-    // todo: read into applescript's docs to see if you can set the desktop for multiple monitors at once (e.g. ..of desktop 1 and 2, and 3)
-    // or if you need to do a .forEach to set each one.
-    let applescript = `osascript -e 'tell application "System Events" to set picture of desktop ${desktopNumber} to ("${filePath}" as POSIX file as alias)'`
-    exec(applescript, (error, standardOut, standardError) => {
-        if (error){
-            console.error(error)
-        } else {
-            // hmm, should we really send a console message at this point? should we just return the
-            // status of execution to the caller and let it handle the decision to message or not message?
-            console.log(chalk.blue(`Desktop ${desktopNumber} set to use background "${filePath}"`))
+        if(!Array.isArray(desktopNumber)){
+            desktopNumber = [desktopNumber]
         }
-
-
+        
+        // todo: read into applescript's docs to see if you can set the desktop for multiple monitors at once (e.g. ..of desktop 1 and 2, and 3)
+        // or if you need to do a .forEach to set each one.
+        let applescript = `osascript -e 'tell application "System Events" to set picture of desktop ${desktopNumber} to ("${filePath}" as POSIX file as alias)'`
+        exec(applescript, (error, standardOut, standardError) => {
+            if (error){
+                reject(new Error(error))
+            } else {
+                // hmm, should we really send a console message at this point? should we just return the
+                // status of execution to the caller and let it handle the decision to message or not message?
+                resolve(`Desktop ${desktopNumber} set to use background "${filePath}"`)
+            }
+        })
     })
 }
 
